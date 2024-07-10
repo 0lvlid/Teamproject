fd51e6a4063a54429f4595029eea4177c4ff450f
0lvlio <omid.1383.po@gmail.com>
2024-07-10 16:04:16 +0330

    2

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
 def greet(name):
     return f"Hello, {name}!"
 
+def current_time():
+    now = datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    greeting = ""
+    time = current_time()
+    if request.method == "POST":
+        user_name = request.form.get("name")
+        greeting = greet(user_name)
+    return render_template("index.html", greeting=greeting, time=time)
+
 if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    print(greet(user_name))
+    app.run(debug=True)
